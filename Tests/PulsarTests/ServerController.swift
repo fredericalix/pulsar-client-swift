8a923cc1ed33a724709ec62705ca21cbbee98b7b
flexlixrup <felixruppert@icloud.com>
2025-02-14 21:06:33 +0100

    tests: fix docker cmd

--- Tests/PulsarTests/ServerController.swift
+++ Tests/PulsarTests/ServerController.swift
@@ -60,7 +60,6 @@ struct ServerController {
 		let dockerCommand = """
 			\(dockerPath) run -d --name pulsar -it \\
 			-p 6650:6650 \\
-			-p 8080:8080 \\
 			--mount source=pulsardata,target=/pulsar/data \\
 			--mount source=pulsarconf,target=/pulsar/conf \\
 			apachepulsar/pulsar:4.0.1 \\
