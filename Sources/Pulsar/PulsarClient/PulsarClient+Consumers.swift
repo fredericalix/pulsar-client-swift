43e5e9c44819bc11841204e275fc85874feeac56
fredericalix <frederic@fredalix.com>
2025-06-22 10:38:32 +0200

    fix: Add missing imports for NIOCore and Logging in PulsarClient extensions

--- Sources/Pulsar/PulsarClient/PulsarClient+Consumers.swift
+++ Sources/Pulsar/PulsarClient/PulsarClient+Consumers.swift
@@ -12,6 +12,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+import NIOCore
+import Logging
+
 extension PulsarClient {
 	/// Creates a new Pulsar consumer.
 	/// - Parameters:
--- Sources/Pulsar/PulsarClient/PulsarClient+Producers.swift
+++ Sources/Pulsar/PulsarClient/PulsarClient+Producers.swift
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+import NIOCore
+
 extension PulsarClient {
 	/// Creates a new Pulsar producer.
 	/// - Parameters:
